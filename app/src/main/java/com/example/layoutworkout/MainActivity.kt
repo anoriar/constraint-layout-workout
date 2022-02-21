a5f5fa6e96f71c1f5015dd980023e1bfdffc501d
anoriar <borisov.alex12@gmail.com>
2022-02-21 13:54:03 +0300

    data binding

--- app/src/main/java/com/example/layoutworkout/MainActivity.kt
+++ app/src/main/java/com/example/layoutworkout/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.layoutworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.layoutworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    fun changeText(view: View) {
+        activityMainBinding.textView?.setText("Binding Data")
     }
 }
